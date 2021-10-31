7cf6664d03842eedd4a1fe46d016980dc9e00673
Matthew Chau <x1n32@students.keele.ac.uk>
2021-10-31 15:10:24 +0000

    Adding back top nav

--- myscripts.js
+++ myscripts.js
@@ -2,7 +2,9 @@
 window.onscroll = function() {scrollFunction()};
 
 function scrollFunction() {
-
+  if (document.body.scrollTop > -1 ) {
+    document.getElementById("navbar").style.top = "0";
+  } else {
     document.getElementById("navbar").style.top = "-50px";
-  
+  }
 }
