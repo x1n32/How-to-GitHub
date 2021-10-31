a9bf920d6e7cb890ccc6bfbca9e3a153cd333ccb
Matthew Chau <x1n32@students.keele.ac.uk>
2021-10-31 15:31:25 +0000

    Testing sticky top nav

--- myscripts.js
+++ myscripts.js
@@ -1,10 +1,23 @@
 // When the user scrolls down 20px from the top of the document, slide down the navbar
-window.onscroll = function() {scrollFunction()};
+/*window.onscroll = function() {scrollFunction()};
 
 function scrollFunction() {
   if (document.body.scrollTop > -1 ) {
     document.getElementById("navbar").style.top = "0";
   } else {
     document.getElementById("navbar").style.top = "-50px";
+  }
+}
+*/
+window.onscroll = function() {stickyScrollFunction()};
+
+var navbar = document.getElementById("navbar");
+var sticky = navbar.offsetTop;
+
+function stickyScrollFunction() {
+  if (window.pageYOffset >= sticky) {
+    navbar.classList.add("sticky")
+  } else {
+    navbar.classList.remove("sticky");
   }
 }
