0f94863d324d122c9bee68894dfb9592cbc896a1
Matthew Chau <x1n32@students.keele.ac.uk>
2021-10-31 15:40:54 +0000

    Testing sticky top nav 4

--- myscripts.js
+++ myscripts.js
@@ -1,14 +1,4 @@
-// When the user scrolls down 20px from the top of the document, slide down the navbar
-/*window.onscroll = function() {scrollFunction()};
 
-function scrollFunction() {
-  if (document.body.scrollTop > -1 ) {
-    document.getElementById("navbar").style.top = "0";
-  } else {
-    document.getElementById("navbar").style.top = "-50px";
-  }
-}
-*/
 // When the user scrolls the page, execute myFunction
 window.onscroll = function() {stickyScrollFunction()};
 
