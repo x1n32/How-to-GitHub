af16a8b7daf40a27f80bc8744ff46120887e05b1
Matthew Chau <x1n32@students.keele.ac.uk>
2021-10-31 15:06:58 +0000

    Testing scroll

--- myscripts.js
+++ myscripts.js
@@ -2,9 +2,7 @@
 window.onscroll = function() {scrollFunction()};
 
 function scrollFunction() {
-  if (document.body.scrollTop >= 0 || document.documentElement.scrollTop >= 0) {
-    document.getElementById("navbar").style.top = "0";
-  } else {
+
     document.getElementById("navbar").style.top = "-50px";
-  }
+  
 }
