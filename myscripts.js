53a14dc69e663ed7156a714c4d4539289f1bd3d5
Matthew Chau <x1n32@students.keele.ac.uk>
2021-11-22 16:01:46 +0000

    added objectives to index. Trying date function

--- myscripts.js
+++ myscripts.js
@@ -27,3 +27,24 @@ function sidebar_open() {
 function sidebar_close() {
   document.getElementById("sidebar").style.display = "none";
 }
+
+
+const date = new Date();
+const currentYear = date.getFullYear();
+const today = date.getDate();
+const currentMonth = date.getMonth() + 1; //runs from 0 to 11, that's why we add 1
+const timeElapsed = Date.now();
+const today = new Date(timeElapsed);
+
+function formatDate(date, format) {
+  const map = {
+      mm: date.getMonth() + 1,
+      dd: date.getDate(),
+      yy: date.getFullYear().toString().slice(-2),
+      yyyy: date.getFullYear()
+  }
+  return format.replace(/mm|dd|yy|yyy/gi, matched => map[matched])
+}
+
+document.getElementById("date").innerHTML = today.toDateString();
+document.getElementById("date2").innerHTML = formatDate(date, dd/mm/yyyy);
